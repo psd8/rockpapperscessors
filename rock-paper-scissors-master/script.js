174d3e9301436b0aaac23a3318e2d93387ef60eb
prashantshah <mrprashantshah@gmail.com>
2020-03-06 21:54:51 +0530

    intial commit

---
+++ rock-paper-scissors-master/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('click',(event) => {
+    if(event.target.matches('button.circle')){
+           
+        animateSlectionDiv();
+        let step2 = document.createElement('div');
+        step2.classList.add('row','d-flex', 'align-items-center', 'justify-content-center', 'auto-groink');
+        step2.innerHTML=`
+            <div class="w-50 white-font">
+                <h3>You Pciked</h3>
+            </div>
+        `;
+    }
+})
+
+function animateSlectionDiv(){
+    document.querySelector('div.selection-on').classList.add('animated', 'zoomOut');
+    hideSelectionDiv();
+}
+function hideSelectionDiv(){
+    document.querySelector('div.selection-on').classList.add('d-none')
+}
